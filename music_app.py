6a3883b958718a9348b7fe631c43de73329ca71a
Greg Chapman <gregc@mac.com>
2024-03-14 22:28:37 +0000

    Add db model

--- music_app.py
+++ music_app.py
@@ -7,17 +7,27 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="gregc",
-    password="0Vw6qWVanF6kcus",
-    hostname="gregc.mysql.pythonanywhere-services.com",
-    databasename="gregc$comments",
+SQLALCHEMY_DATABASE_URI = (
+    'mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}'
+    .format(
+        username='gregc',
+        password='0Vw6qWVanF6kcus',
+        hostname='gregc.mysql.pythonanywhere-services.com',
+        databasename='gregc$comments',
+    )
 )
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 comments = []
 
