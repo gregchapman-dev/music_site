f491b574a60bd5951aa369828533cdb7f5983c1a
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2024-09-15 12:22:14 -0700

    SQLALCHEMY_POOL_RECYCLE=299

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_POOL_RECYCLE = 299
