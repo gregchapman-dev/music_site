c886f84cc74aeccfbde2fcb7a142d7b30a5fb04f
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2024-09-17 11:19:28 -0700

    Add SQLALCHEMY_ENGINE_OPTIONS pool_pre_ping: True.

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_POOL_RECYCLE = 299
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+    }
