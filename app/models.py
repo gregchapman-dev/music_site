7144be788898f6e504dc55f0cb3ed3c396db6525
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2024-09-17 14:05:48 -0700

    Redo database to have larger binaries.

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
+from datetime import datetime
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
 class AnonymousSession(db.Model):  # type: ignore
     sessionUUID: so.Mapped[str] = so.mapped_column(sa.String(128), primary_key=True)
-    musicEngine: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary)
-    mei: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary)
-    humdrum: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary)
-    musicxml: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary)
+    musicEngine: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary(length=(2**32)-1))
+    mei: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary(length=(2**32)-1))
+    humdrum: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary(length=(2**32)-1))
+    musicxml: so.Mapped[bytes | None] = so.mapped_column(db.LargeBinary(length=(2**32)-1))
 
     def __repr__(self):
         return f'<Anon {self.sessionUUID}>'
